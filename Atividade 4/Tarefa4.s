eef97fc84e419279ca9797b7892060f2703de930
Wagner <wagnerweb2010@gmail.com>
2022-07-13 13:16:38 -0300

    tarefa 4

---
+++ Atividade 4/Tarefa4.s
@@ -0,0 +1,17 @@
+begin:  addi $t0, $zero, 0
+		addi $t1, $zero, 20
+loop:	slt  $t2, $a0,   $t1
+		beq  $t2, $zero, finish
+		add  $t0, $t0,   $t1
+		addi $t1, $t1,   3
+		j    loop
+finish:	add  $v0, $t0,   $zero
+
+
+1.c)
+
+v = 0
+for(w = 20; x < w; w += 3){
+    v += w
+}
+z = v
